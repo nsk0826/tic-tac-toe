bf592579fbb7a6481de1ce1d43232e5431018737
nsk0826 <nsk.20160401@gmail.com>
2021-12-13 00:49:19 +0900

    コンポーネントの作成

--- src/index.js
+++ src/index.js
@@ -3,18 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" onClick={function() { console.log('click'); }}>
-          {this.props.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
+}
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares : Array(9).fill(null),
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares : squares});
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)}
+        />
+      )
     }
   
     render() {
