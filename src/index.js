1c8acb5b1ae19d2366a1efbf379765b6f79ea7fa
nsk0826 <nsk.20160401@gmail.com>
2021-12-12 23:39:12 +0900

    検証

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={function() { console.log('click'); }}>
           {this.props.value}
         </button>
       );
