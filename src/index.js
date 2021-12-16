89f164e24bea073af264c4026fc999e5741f335c
nsk0826 <nsk.20160401@gmail.com>
2021-12-16 23:23:45 +0900

    インデントのずれの修正

--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ class Board extends React.Component {
     return (
       <Square 
         value={this.props.squares[i]} 
-        onClick={() => this.props.onClick
-          (i)}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
